81dee57057648330bab9f99710f87e96f46f2eb2
David Vanuhin <2468steeld@gmail.com>
2025-06-07 11:05:32 +0300

    Поля JSON-ответа для GET /module переименованы как на клиенте

--- routers/modules.js
+++ routers/modules.js
@@ -9,10 +9,10 @@ modulesRouter.get('/', async (req, res) => {
   const JSONobj = `json_object(
   'id', Modules.id, 
   'name', Modules.name, 
-  'data', json_group_array(json_object(
+  'terms', json_group_array(json_object(
     'id', Terms.id,  
-    'a', Terms.question, 
-    'q', Terms.answer)
+    'answer', Terms.question, 
+    'question', Terms.answer)
     )
   )`
   const data = await db.all(`
@@ -28,30 +28,5 @@ modulesRouter.get("/:id", async (req, res) => {
   const terms = await db.all(`SELECT question, answer FROM Terms WHERE Terms.module=${req.params.id};`)
   res.json(terms)
 })
-
-/*modulesRouter.post('/add', async (req, res) => {
-  let tasks;
-  try {
-    tasks = utils.remake(req.body.text)
-  } catch (err) {
-    res.render("error", {code: err})
-    res.status(400)
-    res.end()
-    return
-  }
-  try {
-    //const db = await openDb()
-    const result = await db.run(`INSERT INTO Modules (name) VALUES ("${req.body.module}");`)
-    for (let t of tasks) {
-      await db.run(`INSERT INTO Terms (answer, question, module) VALUES ('${t.q}', '${t.a}', ${result.lastID});`)
-    }
-  } catch (err) {
-    console.error(err)
-    res.status(500)
-    res.end()
-    return
-  }
-  res.redirect("/")
-})*/
 
 export default modulesRouter
