1b41ebf97913902a1151acdc3b8c5522af9fa518
David Vanuhin <2468steeld@gmail.com>
2025-06-07 12:32:34 +0300

    Добавлен заголовок cors, чтобы не было ошибки в браузере

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('./static'))
 
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  next()
+})
+
 app.use('/module', modulesRouter)
 
 app.listen(3000, () => console.log("Server started"));
