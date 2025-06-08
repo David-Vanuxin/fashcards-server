5d1fa05180984e1c2135770bb62bad9f8d33c9ce
David Vanuhin <2468steeld@gmail.com>
2025-06-08 16:00:21 +0300

    Теперь GET /module/id выдаёт имя модуля и его термины

--- routers/modules.js
+++ routers/modules.js
@@ -5,28 +5,27 @@ const db = await openDb()
 
 const modulesRouter = express.Router()
 
-modulesRouter.get('/', async (req, res) => {
-  const JSONobj = `json_object(
-  'id', Modules.id, 
+/*modulesRouter.get('/', async (req, res) => {
+
+  const modules = data.map(termList => JSON.parse(termList[JSONobj]))
+  res.json(modules)
+})*/
+
+modulesRouter.get("/:id", async (req, res) => {
+  const JSONobj = `json_object( 
   'name', Modules.name, 
   'terms', json_group_array(json_object(
-    'id', Terms.id,  
+    'id', Terms.id,
     'answer', Terms.question, 
     'question', Terms.answer)
     )
   )`
-  const data = await db.all(`
+  const data = await db.get(`
   select ${JSONobj} 
   from Terms join Modules on Terms.module=Modules.id
+  where Modules.id=${req.params.id}
   group by Modules.id;`)
-
-  const modules = data.map(termList => JSON.parse(termList[JSONobj]))
-  res.json(modules)
-})
-
-modulesRouter.get("/:id", async (req, res) => {
-  const terms = await db.all(`SELECT question, answer FROM Terms WHERE Terms.module=${req.params.id};`)
-  res.json(terms)
+  res.json({ status: "success", data: JSON.parse(data[JSONobj])})
 })
 
 modulesRouter.post('/', async (req, res) => {
